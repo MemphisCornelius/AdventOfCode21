58660f6a4d4debfe152fe0f19724295b896e13bc
MemphisCornelius <m.krich@online.de>
2021-12-05 16:46:16 +0100

    Day01

---
+++ Day01/Day01.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Day01 {
+    internal static class Day01 {
+        public static void Main(string[] args) {
+            string[] lines = System.IO.File.ReadAllLines("../../..//input.txt");
+
+            Console.WriteLine($"Aufgabe01: {Solve01(lines)}");
+            Console.WriteLine($"Aufgabe02: {Solve02(lines)}");
+        }
+
+        static string Solve01(string[] lines) {
+            int counter = 0;
+            for (int i = 0; i < lines.Length - 1; i++) {
+                if (int.Parse(lines[i]) < int.Parse(lines[i + 1])) {
+                    counter++;
+                }
+            }
+
+            return counter.ToString();
+        }
+
+        static string Solve02(string[] lines) {
+            int counter = 0;
+            for (int i = 0; i < lines.Length - 3; i++) {
+                if (int.Parse(lines[i]) + int.Parse(lines[i + 1]) + int.Parse(lines[i + 2]) <
+                    (int.Parse(lines[i + 1]) + int.Parse(lines[i + 2]) + int.Parse(lines[i + 3]))) {
+                    counter++;
+                }
+            }
+
+            return counter.ToString();
+        }
+    }
+}
