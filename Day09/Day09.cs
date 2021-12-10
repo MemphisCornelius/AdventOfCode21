db5acb24de231b7815a2e171d496961e43e048f2
MemphisCornelius <m.krich@online.de>
2021-12-10 13:42:49 +0100

    Day09-2

--- Day09/Day09.cs
+++ Day09/Day09.cs
@@ -34,22 +34,23 @@ namespace Day09 {
             return parsed;
         }
 
-        
+
         static string Solve01(int[,] field) {
             int sum = 0;
-            
+
             Lowpoints(field).ForEach(p => sum += 1 + field[p.X, p.Y]);
 
             return sum.ToString();
         }
-        
+
         static List<Point> Lowpoints(int[,] field) {
             List<Point> list = new();
 
             for (int i = 0; i < field.GetLength(0); i++) {
                 for (int j = 0; j < field.GetLength(1); j++) {
                     int up, right, down, left, current;
-                    (up, right, down, left, current) = (int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue, field[i,j]);
+                    (up, right, down, left, current) =
+                        (int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue, field[i, j]);
 
                     if (i != 0) {
                         up = field[i - 1, j];
@@ -63,9 +64,8 @@ namespace Day09 {
                         left = field[i, j - 1];
                     }
 
-                    if (j != field.GetLength(1) -1 ) {
+                    if (j != field.GetLength(1) - 1) {
                         right = field[i, j + 1];
-
                     }
 
                     if (current < up && current < down && current < left && current < right) {
@@ -89,80 +89,50 @@ namespace Day09 {
             }
 
             foreach (Point point in lowpoints) {
-                
-                    int tiefe = -1;
-                    basinsizes.Add(CheckBasin(fieldWithCheck, point.X, point.Y));
-                
+                basinsizes.Add(CheckBasin(fieldWithCheck, point.X, point.Y));
             }
 
             basinsizes.Sort();
 
-            return (basinsizes[^1] * 
-                    basinsizes[^2] * 
-                    basinsizes[^3]).ToString();
+            return  (basinsizes[^1] * basinsizes[^2] * basinsizes[^3]).ToString();
         }
 
         private static int CheckBasin(int[,,] field, int i, int j) {
-            int basinSize = 0;
+            int basinSize = 1;
             int current = field[i, j, 0];
-            
+
             field[i, j, 1] = 1;
-            
+
             if (field[i, j, 0] == 9) {
                 return 0;
             }
-            
+
             if (i != 0) {
-                if (field[i - 1, j, 1] == 0 && 
-                    field[i - 1, j, 0] + 1 == current) {
-                    //if (field[i - 1, j, 2] != 0) {
-                    //    basinSize += CheckBasin(field, i - 1, j);
-                    //} else {
-                    //    field[i, j, 2] = 1;
-                    //    basinSize += 1 + CheckBasin(field, i - 1, j);
-                    //}
-                    basinSize += 1 + CheckBasin(field, i - 1, j);
+                if (field[i - 1, j, 1] == 0 &&
+                    field[i - 1, j, 0]  > current) {
+                    basinSize += CheckBasin(field, i - 1, j);
                 }
             }
 
             if (i != field.GetLength(0) - 1) {
                 if (field[i + 1, j, 1] == 0 &&
-                    field[i + 1, j, 0] + 1 == current) {
-                    //if (field[i + 1, j, 2] != 0) {
-                    //    basinSize += CheckBasin(field, i + 1, j;
-                    //} else {
-                    //    field[i, j, 2] = 1;
-                    //    basinSize += 1 + CheckBasin(field, i + 1, j);
-                    //}
-                    basinSize += 1 + CheckBasin(field, i + 1, j);
+                    field[i + 1, j, 0] > current) {
+                    basinSize += CheckBasin(field, i + 1, j);
                 }
             }
 
             if (j != 0) {
                 if (field[i, j - 1, 1] == 0 &&
-                    field[i, j - 1, 0] + 1 == current) {
-                    //if (field[i , j-1, 2] != 0) {
-                    //    basinSize += CheckBasin(field, i, j-1);
-                    //} else {
-                    //    field[i, j, 2] = 1;
-                    //    basinSize += 1 + CheckBasin(field, i, j-1);
-                    //}
-                    basinSize += 1 + CheckBasin(field, i, j-1);
+                    field[i, j - 1, 0] > current) {
+                    basinSize += CheckBasin(field, i, j - 1);
                 }
             }
 
-            if (j != field.GetLength(1) -1 ) {
+            if (j != field.GetLength(1) - 1) {
                 if (field[i, j + 1, 1] == 0 &&
-                    field[i, j + 1, 0] + 1 == current) {
-                    //if (field[i , j+1, 2] != 0) {
-                    //    basinSize += CheckBasin(field, i, j+1);
-                    //} else {
-                    //    field[i, j, 2] = 1;
-                    //    basinSize += 1 + CheckBasin(field, i, j+1);
-                    //}
-                    basinSize += 1 + CheckBasin(field, i, j+1);
-                } 
-
+                    field[i, j + 1, 0] > current) {
+                    basinSize += CheckBasin(field, i, j + 1);
+                }
             }
 
             return basinSize;
