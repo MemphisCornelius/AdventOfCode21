8cec83931df9055ebe8d136981a5942b688032c2
MemphisCornelius <m.krich@online.de>
2021-12-15 20:00:40 +0100

    Day15-1

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -12,14 +12,14 @@ namespace Day14 {
 
             sr.Close();
 
-            ParseInput(values, out List<(Point, Dictionary<Point, int>)> graph);
+            ParseInput(values, out Dictionary<Point, Dictionary<Point, int>> graph);
 
-            //Console.WriteLine($"Aufgabe01: {Solve01()}");
+            Console.WriteLine($"Aufgabe01: {Solve01(graph)}");
 
             //Console.WriteLine($"Aufgabe02: {Solve02()}");
         }
 
-        private static void ParseInput(List<string> values, out List<(Point, Dictionary<Point, int>)> graph) {
+        private static void ParseInput(List<string> values, out Dictionary<Point, Dictionary<Point, int>> graph) {
             int[,] map = new int[100, 100];
             graph = new();
 
@@ -33,24 +33,58 @@ namespace Day14 {
             for (int i = 0; i < map.GetLength(0); i++) {
                 for (int j = 0; j < map.GetLength(1); j++) {
                     Dictionary<Point, int> follow = new();
-                    
+
                     if (i != 0) {
-                        follow.Add(new Point(i-1, j), map[i-1, j]);
+                        follow.Add(new Point(i - 1, j), map[i - 1, j]);
                     }
+
                     if (j != 0) {
-                        follow.Add(new Point(i, j-1), map[i, j-1]);
+                        follow.Add(new Point(i, j - 1), map[i, j - 1]);
                     }
+
                     if (i != map.GetLength(0) - 1) {
-                        follow.Add(new Point(i+1, j), map[i+1, j]);
+                        follow.Add(new Point(i + 1, j), map[i + 1, j]);
                     }
-                    if (j != map.GetLength(1) -1) {
-                        follow.Add(new Point(i, j+1), map[i, j+1]);
+
+                    if (j != map.GetLength(1) - 1) {
+                        follow.Add(new Point(i, j + 1), map[i, j + 1]);
                     }
-                    
-                    graph.Add((new Point(i, j), follow));
+
+                    graph.Add(new Point(i, j), follow);
                 }
             }
         }
-        
+
+        private static string Solve01(Dictionary<Point, Dictionary<Point, int>> graph) {
+            return Dijkstra(graph, 99, 99).ToString();
+        }
+
+        static int Dijkstra(Dictionary<Point, Dictionary<Point, int>> graph, int endX, int endY) {
+            Dictionary<Point, int> distanceFromStart = new();
+            Queue<Point> queue = new();
+            queue.Enqueue(new Point(0, 0));
+
+            foreach (KeyValuePair<Point, Dictionary<Point, int>> node in graph) {
+                distanceFromStart.Add(node.Key, int.MaxValue);
+            }
+
+            distanceFromStart[new Point(0, 0)] = 0;
+
+            while (queue.Count > 0) {
+                Point current = queue.Dequeue();
+
+                foreach (KeyValuePair<Point, int> follow in graph[current]) {
+                    int distance = (distanceFromStart[current] + follow.Value);
+
+                    if (distance < distanceFromStart[follow.Key]) {
+                        distanceFromStart[follow.Key] = distance;
+
+                        queue.Enqueue(follow.Key);
+                    }
+                }
+            }
+
+            return distanceFromStart[new Point(endX, endY)];
+        }
     }
 }
